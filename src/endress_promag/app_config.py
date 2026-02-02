88831af534e815860fde7bcba2f5418cb2c64cb1
Tom Wyatt <tom@span-eng.com>
2026-02-02 20:22:16 +1000

    Published to doover 2.0

--- src/endress_promag/app_config.py
+++ src/endress_promag/app_config.py
@@ -1,20 +1,25 @@
 from pathlib import Path
 
 from pydoover import config
+from pydoover.docker.modbus import ModbusConfig
 
 
 class EndressPromagConfig(config.Schema):
     def __init__(self):
         self.meter_name = config.String("Meter Name", default="Meter 1")
 
-        self.eh_meter_host = config.String("EH Meter Host")
-        self.eh_meter_port = config.Integer("EH Meter Port", default=80)
+        self.max_flow = config.Number("Max Flow", default=None, description="Maximum flow in m3/h. Only used for display purposes. Leave blank to disable.")
+
+        self.eh_meter_host = config.String("EH Meter IP Host. Default is 192.168.1.212", default=None)
+        self.eh_meter_port = config.Integer("EH Meter IP Port", default=80)
         self.eh_meter_password = config.String("EH Meter Password", default="0000")
 
-        self.max_flow = config.Number("Max Flow", default=None, description="Maximum flow in m3/h. Only used for display purposes. Leave blank to disable.")
-        self.eh_meter_serial_number = config.String("EH Meter Serial Number", default=None, description="OPTIONAL: If provided, any meter not matching this serial number will be ignored")
+        self.modbus_id = config.Integer("Modbus ID", default=None, description="Modbus ID. Default is 1. If this is set, the EH Meter IP Host and Port will be ignored and the Modbus connection will be used instead.")
+        self.modbus_config = ModbusConfig()
 
+        self.eh_meter_serial_number = config.String("EH Meter Serial Number", default=None, description="OPTIONAL: If provided, any meter not matching this serial number will be ignored")
         self.no_comms_timeout = config.Integer("No Comms Timeout", default=10, description="Time in minutes after which the meter is considered to be offline")
+
 
 
 def export():
